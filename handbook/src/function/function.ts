dc4056ce7cc7ae51b4a18609349fc1fc02b75cf9
Reza Mirjahanian <rmirjahanian@gmail.com>
2022-10-28 10:14:12 +0330

    func

--- handbook/src/function/function.ts
+++ handbook/src/function/function.ts
@@ -42,3 +42,33 @@ interface CallOrConstruct {
 
     (n?: number): number;
 }
+
+// Generic
+function firstElement<Type>(arr: Type[]): Type  {
+    return arr[0];
+}
+
+// s is of type 'string'
+const s = firstElement(["a", "b", "c"]);
+// n is of type 'number'
+const n = firstElement([1, 2, 3]);
+// u is of type undefined
+const u = firstElement([]);
+
+
+// Constraints
+
+function longest<Type extends { length: number }>(a: Type, b: Type) {
+    if (a.length >= b.length) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+// longerArray is of type 'number[]'
+const longerArray = longest([1, 2], [1, 2, 3]);
+// longerString is of type 'alice' | 'bob'
+const longerString = longest("alice", "bob");
+// Error! Numbers don't have a 'length' property
+const notOK = longest(10, 100);
